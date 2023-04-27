f241e06dab318118c959726379a98a762472e330
Tada-1234 <tadalaangellagomonda@gmail.com>
2023-04-27 14:06:47 -0700

    Final tasks

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,26 +7,22 @@
  */
 void print_diagonal(int n)
 {
-	int i, j;
-	char back_slash = 92;
+	int a, j;
 
 	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
 		{
-			for(j = 0; j < n; j++)
+			for (a = 0; a < j; a++)
 			{
-				if (i == j)
-				{
-					_putchar(back_slash);
-					_putchar('\n');
-				}
-				else
-				{
-					_putchar(' ');
-				}
+				_putchar(' ');
 			}
+			_putchar('\\');
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+void print_triangle(int size);
+void print_square(int size);
 int _putchar(char c);
 int _isupper(int c);
 int mul(int a, int b);
