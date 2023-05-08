9122bedb66911badfd74a540f60e374edca532c7
Tada-1234 <tadalaangellagomonda@gmail.com>
2023-05-08 15:59:58 -0700

    Last commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,3 +5,5 @@ char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 int _putchar(char c);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
